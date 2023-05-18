1f9cff7a2bce4d035981da8a63edbaa000b5855a
qige2016 <286882998@qq.com>
2023-05-18 15:56:24 +0800

    Tweak

---
+++ DotNet/Next.Backend/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Next.Backend
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
