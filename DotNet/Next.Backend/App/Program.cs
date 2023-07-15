0f891b43a11f5025245b6f87bea35271199f0c4b
qige2016 <286882998@qq.com>
2023-07-16 00:20:25 +0800

    Update

---
+++ DotNet/Next.Backend/App/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Next.Backend.Extensions;
+
+namespace Next.Backend.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((_, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                    config.SetBasePath(EnvironmentExtension.GetCurrentEnvironementDirectory());
+                    config.AddYamlFile("config/server.yml", optional: false, reloadOnChange: true);
+                }).ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    services.Configure<ServerOptions>(hostContext.Configuration.GetSection("server"));
+                    services.Configure<CoreCacheServerOptions>(hostContext.Configuration.GetSection("core-cache-server"));
+
+                    services.AddAccountPersistance(hostContext.Configuration.GetSection("account-database")
+                        .Get<DatabaseOptions>());
+                    services.AddGamePersistance(hostContext.Configuration.GetSection("game-database")
+                        .Get<DatabaseOptions>());
+                    
+                    services.AddSingleton<IClusterCache, CoreCacheServer>(serviceProvider => serviceProvider.GetRequiredService<CoreCacheServer>());
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddConsole();
+                    builder.AddLoggingFilters();
+                })
+        }
+    }
+}
---
+++ DotNet/Next.Backend/Configuration/ConfigurationConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace Next.Backend.Configuration;
+
+/// <summary>
+/// Contains contants related to the configuration.
+/// </summary>
+public sealed class ConfigurationConstants
+{
+    /// <summary>
+    /// Gets the default configuration path when Next is running inside a container.
+    /// </summary>
+    public static readonly string DefaultDockerConfigurationPath = "/var/next";
+
+    /// <summary>
+    /// Gets the Next docker configuration environment key.
+    /// </summary>
+    public static readonly string DockerConfigurationKey = "NEXT_CONFIGURATION";
+
+    /// <summary>
+    /// Prevents from creating a <see cref="ConfigurationConstants"/> instance from outside.
+    /// </summary>
+    private ConfigurationConstants()
+    {
+    }
+}
